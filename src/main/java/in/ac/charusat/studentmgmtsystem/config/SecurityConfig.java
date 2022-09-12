49d52bcf7d55de9998c02ca1a8de4de162c01c65
sohamkhanna <20it056@charusat.edu.in>
2022-09-12 14:13:52 +0530

    Cors filer is added to handle axios request

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -1,17 +1,19 @@
 package in.ac.charusat.studentmgmtsystem.config;
 
-import net.bytebuddy.asm.Advice;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
-
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -21,32 +23,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.jdbcAuthentication().dataSource(dataSource);
-
 //        auth.jdbcAuthentication().dataSource(dataSource)
 //                .withDefaultSchema()
-//                .withUser("admin")
-//                .password("pass")
-//                .roles("ADMIN", "USER")
-//                .and()
-//                .withUser("user")
-//                .password("pass")
-//                .roles("USER");
-
-
-//        auth.inMemoryAuthentication()
-//                .withUser("admin")
-//                .password("pass")
-//                .roles("ADMIN", "USER")
-//                .and()
-//                .withUser("user")
-//                .password("pass")
-//                .roles("USER");
+//                .withUser(
+//                        User.withUsername("admin")
+//                                .password("admin")
+//                                .roles("ADMIN")
+//                )
+//                .withUser(
+//                        User.withUsername("user")
+//                                .password("user")
+//                                .roles("USER")
+//                );
+       auth.inMemoryAuthentication()
+               .withUser("admin")
+               .password("admin")
+               .roles("ADMIN")
+               .and()
+               .withUser("user")
+               .password("user")
+               .roles("USER");
     }
 
-    @Override
+    //    @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/login").permitAll()
+//                .anyRequest().authenticated().and()
+//                .formLogin();
+//        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+        ;
         http.csrf().disable();
         http.headers().frameOptions().disable();
     }
@@ -56,4 +67,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 }
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into users values ('admin', 'pass', true);
-insert into users values ('user', 'pass', true);
-
-insert into authorities values ('user', 'ROLE_USER');
-insert into authorities values ('admin', 'ROLE_ADMIN');
